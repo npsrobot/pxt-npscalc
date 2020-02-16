8739583be326f0dcd9b66cda4a68b5241e66b627
npsrobot <61123765+npsrobot@users.noreply.github.com>
2020-02-17 05:18:44 +0700

    Update microbitRayong.ts

--- microbitRayong.ts
+++ microbitRayong.ts
@@ -1,20 +1,20 @@
 /**
- * ???????????????????????? Javascript  
+ * สร้างบล็อกคำสั่งด้วยภาษา Javascript  
  * Amnart P.
  * NPS ROBOTICS COMPUTER RAYONG  
  * https://www.facebook.com/NPSProgrammerRayong/
  */
 
 enum npsOper {
-    //% block="???"
+    //% block="บวก"
     op1,
-    //% block="??"
+    //% block="ลบ"
     op2,
-    //% block="???"
+    //% block="คูณ"
     op3,
-    //% block="???"
+    //% block="หาร"
     op4,
-    //% block="???????"
+    //% block="ยกกำลัง"
     op5,
     //% block="mod"
     op6
@@ -23,17 +23,17 @@ enum npsOper {
 //% block="microbit:Rayong" weight=150 color=#1111ff icon="\uf17b"
 namespace microbitRayong {
     /**
-     * ????????????? 2 ?????
-     * ?????????????????? 0-10
+     * โปรแกรมบวกเลข 2 จำนวน
+     * เลือกตัวเลขตั้งแต่ 0-10
      */
-    //% block="?????? ??????? %v1| ?????? %v2"
+    //% block="บวกเลข ตัวตั้ง %v1| ตัวบวก %v2"
     //% v1.min=0 v1.max=10 v2.min=0 v2.max=10
     //% weight=100 color=#1111ff 
     export function plusNumber(v1: number, v2: number): void {
         basic.showNumber(v1 + v2)
     }
 
-    //% blockId="Calculator" block="???????????? %v1| %npsOper | %v2"
+    //% blockId="Calculator" block="หาผลลัพธ์ของ %v1| %npsOper | %v2"
     //% weight=90
     export function Calculate(v1: number, o: npsOper, v2: number): void {
         if (o == npsOper.op1) {
